cc8b6a28299dc3b70e61c42135b49666c419f29a
luisagosti <domgolfinho@gmail.com>
2022-12-19 10:34:42 +0000

    teste

--- controller.py
+++ controller.py
@@ -1,6 +1,12 @@
+<<<<<<< Updated upstream
 def colocar_peca(n,x,y):
     for n in range(1,x):
         for n in range(1,y):
             if 
+=======
+# def colocar_peca(n,x):
+#     for n in range(1,n*1.5):
+#         for n in range(1,n*1.25):
+>>>>>>> Stashed changes
             
 
