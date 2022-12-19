d5b351f46750ff0d6decba119a0bf251b8b4c417
luisagosti <domgolfinho@gmail.com>
2022-12-19 09:50:29 +0000

    teste

---
+++ view.py
@@ -0,0 +1,8 @@
+from controller import * 
+import os
+
+def main():
+    os.system('cls')
+    print("RJ - Registar jogador\nEJ - Remover jogador\nLJ - Listar jogadores\nIJ - Iniciar jogo\nDJ - Detalhes do jogo\n")
+
+main()
