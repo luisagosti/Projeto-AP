ffb783c22c6b2872188d037a4783125bee3e54de
joaoliveira12345 <121021610+joaoliveira12345@users.noreply.github.com>
2022-12-23 15:52:54 +0000

    c

--- controller.py
+++ controller.py
@@ -1,18 +1,15 @@
-<<<<<<< Updated upstream
+
 def colocar_peca(n,x,y):
     for n in range(1,x):
         for n in range(1,y):
-<<<<<<< Updated upstream
-            if 
-=======
+            pass
 # def colocar_peca(n,x,y,a,p):
 #     for n in range(1,x):
 #         for n in range(1,y):
 #            if  
 #              a.append()
->>>>>>> Stashed changes
-            
 
-=======
-            if
->>>>>>> Stashed changes
+def remover(x,y):
+    x.remove(y)
+def adicionar(x,y):
+    x.append(y)
--- view.py
+++ view.py
@@ -16,11 +16,50 @@ def main():
         G - Gravar
         L - Ler
     ''')
+    lista = []
+    z = len(lista)
     menu = input()
-    # n = int(input("Numero de pecas em linha necessarias para a vitoria"))
-    # x = round(n*1.5)
-    # y = round(n*1.25)
-    # a = {}
-    # p = 
+    if menu == 'Rj':
+        print("Insira o nome do jogardor")
+        x = input()
+        adicionar(lista,x)
+    elif menu == 'EJ':
+        if len(lista) == 0:
+            print("Opcao invalida")  
+        else:
+            print('Insira o nome do jogador que deseja eliminar')
+            y = input()
+            remover(lista,y)
+    elif menu == 'LJ':
+        jogador_1 = [lista[z-1]]
+        jogador_2 = [lista[z]]
+        print(f'''~
+            jogador 1 é {x}
+            jogador 2 é {y}
+        ''')
+    elif menu == 'IJ':
+        # n = int(input("Numero de pecas em linha necessarias para a vitoria"))
+        # x = round(n*1.5)
+        # y = round(n*1.25)
+        # a = {}
+        # p = 
+        pass
+    elif menu == 'DJ':
+        pass
+    elif menu == 'D':
+        pass
+    elif menu == 'CP': 
+        pass
+    elif menu == 'V':
+        pass
+    elif menu == 'G':
+        pass
+    elif menu == 'L':
+        pass
+
+
+                  
+        
+   
     
 main()
