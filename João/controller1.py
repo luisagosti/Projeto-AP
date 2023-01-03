95c4cd17ac23d2bd4c10c422ad39f2364c06f989
joaoliveira12345 <121021610+joaoliveira12345@users.noreply.github.com>
2023-01-03 13:26:48 +0000

    c

---
+++ João/controller1.py
@@ -0,0 +1,73 @@
+def colocar_peca(n,x,y):
+    for n in range(1,x):
+        for n in range(1,y):
+            pass
+# def colocar_peca(n,x,y,a,p):
+#     for n in range(1,x):
+#         for n in range(1,y):
+#            if  
+#              a.append()
+def adicionar_jogador(lista, jogador):
+    if jogador not in lista:
+        lista.append(jogador)
+        return True
+
+def eliminar_jogador(lista, jogador):
+    if jogador in lista:
+        lista.remove(jogador)
+        return True
+
+def registar_Jogadores(nome_Jogador, lista_Jogadores):
+    if nome_Jogador in lista_Jogadores:
+        x = ("Jogador existente.")
+        return x
+    else:
+        lista_Jogadores.append(nome_Jogador)
+        x = ("Jogador registado com sucesso.")
+        return x
+
+def remover_Jogadores(lista_Jogadores, nome_apagarJogador, decorrer_Jogo):
+    if nome_apagarJogador in lista_Jogadores:
+        lista_Jogadores.remove(nome_apagarJogador)
+        x = ("Jogador removido com sucesso.")
+        return x
+    elif decorrer_Jogo == 1 and nome_apagarJogador in lista_Jogadores:
+        x = ("Jogador participa no jogo em curso.")
+        return x
+    else:
+        x = ("Jogador não existente.")
+        return x
+
+def movimento_Jogada(jogada, coluna, tabuleiro):
+    coluna -= 1
+    for i in range(5, -1, -1):
+        if tabuleiro[i][coluna] == ' ':
+            tabuleiro[i][coluna] = jogada
+            return
+    x = ("A coluna encontra-se completa, escolhe outra coluna.")        
+    return x
+
+def verificar_Vitoria(jogada, tabuleiro, coluna):
+    # Verifica vitória horizontal
+    for linha in tabuleiro:
+        if linha.count(jogada) == 4:
+            return True
+    # Verifica vitória vertical
+    for coluna in range(7):
+        if tabuleiro[0][coluna] == jogada and tabuleiro[1][coluna] == jogada and tabuleiro[2][coluna] == jogada and tabuleiro[3][coluna] == jogada:
+            return True
+    # Verifica vitória diagonal
+    for linha in range(3):
+        for coluna in range(4):
+            if tabuleiro[linha][coluna] == jogada and tabuleiro[linha+1][coluna+1] == jogada and tabuleiro[linha+2][coluna+2] == jogada and tabuleiro[linha+3][coluna+3] == jogada:
+                return True
+    for linha in range(3):
+        for coluna in range(4):
+            if tabuleiro[linha+3][coluna] == jogada and tabuleiro[linha+2][coluna+1] == jogada and tabuleiro[linha+1][coluna+2] == jogada and tabuleiro[linha][coluna+3] == jogada:
+                return True
+    return False
+
+def LJ(lista,z):
+    for lista in range(0,z):
+        return lista
+        
---
+++ João/view.py
@@ -0,0 +1,182 @@
+from controller1 import *
+import os
+
+def main():
+
+    # w (comprimento_Grelha) — Comprimento da grelha, em peças, onde w ∈ N
+    comprimento_Grelha = 0
+
+    # h (altura_Grelha) — Altura da grelha em peças, onde h ∈ N, [w/2] ≤ h ≤ w
+    altura_Grelha = 0
+
+    # n (tamanho_Sequencia) — Número de peças em linha para determinar a vitória, onde n ∈ N, n ≤ w
+    tamanho_Sequencia = ()
+
+    # S (tamanho_PecasEspeciais) — Conjunto de tamanhos de peças especiais disponível para cada jogador, onde ∀s ∈ S : S ∈ N, s < n
+    tamanho_PecasEspeciais = []
+
+    # Lista jogadores
+    lista_Jogadores = []
+    z = len(lista_Jogadores)
+
+    # Verificação de jogo em curso
+    decorrer_Jogo = 0
+
+    # Tabuleiro
+    tabuleiro = [[' ' for _ in range(comprimento_Grelha)] for _ in range(altura_Grelha)]
+
+    # Variável com valores temporários
+    temp_var = ''
+
+
+    os.system('cls')
+    print('''
+        RJ - Registar jogador
+        EJ - Remover jogador
+        LJ - Listar jogadores
+        IJ - Iniciar jogo
+        DJ - Detalhes do jogo
+        D - Desistir
+        CP - Colocar peça
+        V - Visualizar resultado
+        G - Gravar
+        L - Ler
+        X - Sair
+        ''')
+    op1 = input("Digite uma opção: ")   # Opção 1
+    
+    # Enquanto a "Opção 1" não igualar nenhuma das opções do array, o programa irá continuar a perguntar por uma opção.
+    # Ele passa quando a "Opção 1" for equivalente a alguma das opções dentro do array
+    while op1 not in ["RJ", "EJ", "LJ", "IJ", "DJ", "D", "CP", "V", "G", "L", "X"]:
+        os.system('cls')
+        print("Instrução inválida.")
+        print('''
+            RJ - Registar jogador
+            EJ - Remover jogador
+            LJ - Listar jogadores
+            IJ - Iniciar jogo
+            G - Gravar
+            L - Ler
+            X - Sair
+            ''')
+    op1 = input("Digite uma opção: ")   # Opção 1
+
+    # Enquanto a "Opção 1" for diferente de X
+    while op1 != "X":
+
+        # RJ - Registar jogador
+        if op1 == "RJ":
+            os.system('cls')
+            num_Jogadores = eval(input("Quantos jogadores pretende registar: "))
+            for i in range(num_Jogadores):
+                nome_Jogador = input("Digite o nome do " + str(i + 1) + "º jogador: ")
+                os.system('cls')
+                print(registar_Jogadores(nome_Jogador, lista_Jogadores))
+
+            print('''
+                RJ - Registar jogador
+                EJ - Remover jogador
+                LJ - Listar jogadores
+                IJ - Iniciar  jogo
+                G - Gravar
+                L - Ler
+                X - Sair
+                ''')
+            op1 = input("Digite uma opção: ")   # Opção 1
+        
+        # EJ - Remover jogador
+        elif op1 == "EJ":
+            os.system('cls')
+            if len(lista_Jogadores) == 0:
+                print("Não existem jogadores registados.")
+            else:
+                for i in range(len(lista_Jogadores)):
+                    print(lista_Jogadores[i])
+                nome_apagarJogador = input("Digite o nome do utilizador que pretende apagar")
+                print(remover_Jogadores(lista_Jogadores, nome_apagarJogador, decorrer_Jogo))
+
+
+        # LJ - Listar jogadores
+        elif op1 == "LJ":
+            os.system('cls')
+            LJ(lista_Jogadores,z)
+        # IJ - Iniciar jogo
+        elif op1 == "IJ":
+            os.system('cls')
+            if len(lista_Jogadores) < 2:
+                print("Jogadores insuficientes.")
+            else:
+                print("Bem vindo ao jogo")
+                tamanho_Sequencia = eval(input("Para comecar digite o numero de pecas em linha necesserarias para a vitoria\n"))
+                comprimento_Grelha = eval(input("Indique o comprimento da grelha: "))
+                altura_Grelha = eval(input("Indique a altura da grelha: "))
+                while menu_2 !='D' or verificar_Vitoria == False:
+                    menu_2 = input('''Escolha uma opcao
+                    LJ - Listar jogadores
+                    DJ - Detalhes do jogo
+                    D - Desistir
+                    CP - Colocar peça
+                    V - Visualizar resultado
+                    G - Gravar
+                    L - Ler
+                    ''')
+                    if menu_2 == 'LJ':
+                        os.system('cls')
+                        LJ(lista_Jogadores,z)
+
+                    nome_Jogador1 = input("Indique o nome do 1º jogador: ")
+                    while nome_Jogador1 not in lista_Jogadores:
+                        print("Jogador não registado.")
+                        nome_Jogador1 = input("Indique o nome do 1º jogador: ")
+
+                    nome_Jogador2 = input("Indique o nome do 2º jogador: ")
+                    while nome_Jogador2 not in lista_Jogadores:
+                        print("Jogador não registado.")
+                        nome_Jogador2 = input("Indique o nome do 1º jogador: ")
+
+                    
+                    while altura_Grelha < comprimento_Grelha/2 and altura_Grelha > comprimento_Grelha:
+                        os.system('cls')
+                        print("Dimensões de grelha invalidas.")
+                        comprimento_Grelha = input("Indique o comprimento da grelha: ")
+                        altura_Grelha = input("Indique a altura da grelha: ")
+
+                    # Exibir o tabuleiro
+                    for i in range(comprimento_Grelha):
+                        temp_var = temp_var + str(str(i + 1) + ' ')
+                    print(temp_var)
+
+                    for linha in tabuleiro:
+                        print('|'.join(linha))
+
+                    jogada = nome_Jogador1
+
+                    coluna = int(input(f"{jogada}, escolhe uma coluna: "))
+                    movimento_Jogada(jogada, coluna, tabuleiro)
+                    if verificar_Vitoria(jogada, tabuleiro, coluna):
+                        for i in len(comprimento_Grelha):
+                            temp_var.append(str(i + ' '))
+                        print(temp_var)
+
+                        for linha in tabuleiro:
+                            print('|'.join(linha))
+                        print(f"{jogada} Venceu!")
+                        break
+
+                    jogada = nome_Jogador2 if jogada == nome_Jogador1 else nome_Jogador1
+
+        # G - Gravar
+        elif op1 == "G":
+            os.system('cls')
+
+        # L - Ler                        Ler ficheiro
+        elif op1 == "L":
+            os.system('cls')
+
+        # X - Sair
+        else:
+            #print("Obrigado por jogar")
+            break
+
+
+main()
